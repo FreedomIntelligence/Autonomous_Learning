d3d47305a0c02d74c1d452cc92bb22242d0af889
Ke Ji <39404568+1KE-JI@users.noreply.github.com>
2024-06-02 09:41:46 +0800

    Add files via upload

---
+++ run_scripts/run_open_book_QA.sh
@@ -0,0 +1,4 @@
+accelerate launch --main_process_port 29502 \
+    --config_file recipes/accelerate_configs/deepspeed_zero3_sft.yaml \
+    my_scripts/run_open_book.py \
+    recipes/run_AL/open_book/config_full_QA.yaml
